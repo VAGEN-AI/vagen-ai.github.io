e67e6de3bb447ce776f66bbd1c7fbc4073d7b1ea
Kangrui Wang <kangruiwang.cs@gmail.com>
2025-06-25 03:32:42 -0700

    fix: set Astro base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://vagen-ai.github.io",
-    base: '/',
+    base: '/vagen.github.io/',
     build: {
         assets: "_astro"
     }
