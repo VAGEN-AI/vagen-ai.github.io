f25865a4d9126873f398a8fcb85f3933daee1f70
Kangrui Wang <kangruiwang.cs@gmail.com>
2025-07-19 16:44:56 -0700

    minor

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://vagen-ai.github.io",
-    base: '/vagen-project/',
     build: {
         assets: "_astro"
     }
